7639c6403b5d0ef7a59fa564336900c75d043297
Alan Sanchez <sanchezalan95@gmail.com>
2019-01-27 14:33:33 -0600

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,17 +28,19 @@ socket.on('newLocationMessage', function(message){
     li.text(`${message.from}: `);
     a.attr('href', message.url);
     li.append(a);
-    $('#messages').append(li);
+    $('#messages').append(li);  
 });
 
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
 
+    var messageTextbox = $('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, function() {
-
+        //messageTextbox.val('');
     });
 });
 
@@ -49,12 +51,16 @@ locationButton.on('click', function(){
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location');
     })
 
